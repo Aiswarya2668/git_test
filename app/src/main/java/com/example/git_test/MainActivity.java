b500c44c6495a697a035275f9973161e8a5031b8
Aiswarya2668 <aiswaryaravi2668@gmail.com>
2020-01-29 11:51:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/git_test/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.git_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      button =(Button)findViewById(R.id.button);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "name", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+
+
+}
